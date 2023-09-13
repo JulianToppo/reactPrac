6ec103345100a931159a74f80dfc81b126204a4a
JulianToppo <juliantoppo95@gmail.com>
2023-09-13 17:13:44 +0530

    addition of states for amount ,date,title

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,32 +1,73 @@
 import React, { useState } from "react";
+import './ExpenseForm.css'
+import './NewExpense.css'
 
 export default function ExpenseForm() {
-  const [title, setTitle] = useState();
-  const [amount, setAmount] = useState("");
+  const [enteredTitle, setTitle] = useState("");
+  const [enteredAmount, setAmount] = useState("");
+  const [enteredDateOfExpense, setDateOfExpense] = useState("");
 
   function changeTitle(e) {
-    console.log(e.target.value)
+    console.log(e.target.value);
+    setTitle(e.target.value);
   }
 
-  function printOutput() {
-    console.log(title);
+  function changeAmount(e) {
+    console.log(e.target.value);
+    setAmount(e.target.value);
+  }
+
+  function changeDate(e) {
+    console.log(e.target.value);
+    setDateOfExpense(e.target.value);
   }
 
   return (
-    <form>
-      <label htmlFor="title">Expense Title:</label>
-      <input
-        id="title"
-        value={title}
-        onChange={changeTitle}
-        type="text"
-      ></input>
-      <label htmlFor="amount">Amount:</label>
-      <input id="amount" type="number"></input>
-      <label htmlFor="date">Date:</label>
-      <input type="date" id="date"></input>
+    <div className="new-expense">
+      <form>
+        <div className="new-expense__controls">
+          <div className="new-expense__controls">
+            <label className="new-expense__control label" htmlFor="title">
+              Expense Title:
+            </label>
+            <input
+              className="new-expense__control input"
+              id="title"
+              value={enteredTitle}
+              onChange={changeTitle}
+              type="text"
+            ></input>
+          </div>
 
-      <button type="submit">Enter Details</button>
-    </form>
+          <div className="new-expense__controls">
+            <label className="new-expense__control label" htmlFor="amount">
+              Amount:
+            </label>
+            <input
+              className="new-expense__control input"
+              id="amount"
+              value={enteredAmount}
+              type="number"
+              onChange={changeAmount}
+            ></input>
+          </div>
+
+          <div className="new-expense__controls">
+            <label className="new-expense__control label" htmlFor="date">
+              Date:
+            </label>
+            <input
+              className="new-expense__control input"
+              type="date"
+              id="date"
+              value={enteredDateOfExpense}
+              onChange={changeDate}
+            ></input>
+          </div>
+
+          <button type="submit">Enter Details</button>
+        </div>
+      </form>
+    </div>
   );
 }
